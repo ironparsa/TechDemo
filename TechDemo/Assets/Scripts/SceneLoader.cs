5beb28e7c1280c815c9a1520b7160240f6db870a
ironparsa <ironparsa@gmail.com>
2021-03-30 18:22:03 -0400

    Pause menu and main menu improvements
    -Pause menu animation
    -main menu logic
    -pause menu button logic

--- TechDemo/Assets/Scripts/SceneLoader.cs
+++ TechDemo/Assets/Scripts/SceneLoader.cs
@@ -6,21 +6,27 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    //switch to "2d" scene
     public void _2dScene()
     {
         Debug.Log("Switching to 2D Scene");
         SceneManager.LoadScene("2d");
     }
+    //switch to "3d" scene
     public void _3dScene()
     {
         Debug.Log("Switching to 3D Scene");
         SceneManager.LoadScene("3d");
-    }    
+    }
+    //switch to "MainMenu" scene
     public void MainMenuScene()
     {
         Debug.Log("Switching to Main Menu");
+        // to make sure the game is not paused when returning to the mainmenu
+        Time.timeScale = 1f;
         SceneManager.LoadScene("Main");
     }
+    //switch to quit the game
     public void QuitGame() 
     {
         Debug.Log("Quitting game");
