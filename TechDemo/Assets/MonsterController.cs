bc1e60de48ae6508886db7b27089cc773706ecc7
ironparsa <ironparsa@gmail.com>
2021-04-01 21:20:22 -0400

    NPC/AI development
    -REMOVED Q functionality for the AI due to an issue with it.
    - Monsters spawn at an adjustable rate within Unity
    - Monsters spawn at a random location within the back of the maze
    - Max monsters is adjustable within Unity.
    
    -Added breakable boxes at the end of the level.

--- TechDemo/Assets/MonsterController.cs
+++ TechDemo/Assets/MonsterController.cs
@@ -10,26 +10,61 @@ public class MonsterController : MonoBehaviour
 
     public bool followPlayer = true;
 
+    public float spawnTimer = 10f;
+    public float spawnAmount = 25f;
+    public float amountSpawned;
+
+    public static RaycastHit hitSpot;
+
+    public GameObject monster;
     public NavMeshAgent agent;
+
+    public static float monsterSpawn = 1;
+
+    private void Start()
+    {
+        if(monsterSpawn == 1)
+        {
+            StartCoroutine(SpawnMonster(spawnTimer));
+            monsterSpawn++;
+        }
+    }
 
     void Update()
     {
+        agent.SetDestination(Player.transform.position);
+        //commented code below is for Q to move the beans around, but it's broken try to fix later.
+        /*
         if (followPlayer == true)
         {
             agent.SetDestination(Player.transform.position);
         }
-
+        */
+        /*
         if (Input.GetKeyDown("q"))
-        {
-            followPlayer = !followPlayer;
+        {            
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
             if (Physics.Raycast(ray, out hit))
             {
-                //move monster
+                hitSpot = hit;
                 agent.SetDestination(hit.point);
             }
+            followPlayer = !followPlayer;
+        }
+        */
+    }
+
+    IEnumerator SpawnMonster(float secs)
+    {
+        yield return new WaitForSeconds(secs);
+        var position = new Vector3(Random.Range(-20f, 12f), 1.88f, Random.Range(-31f, -28f));
+        Instantiate(monster, position, Quaternion.identity);
+        if (spawnAmount >= amountSpawned)
+        {
+            amountSpawned++;
+            StartCoroutine(SpawnMonster(spawnTimer));
         }
     }
 }
--- TechDemo/Assets/Scripts/Character/Gun.cs
+++ TechDemo/Assets/Scripts/Character/Gun.cs
@@ -32,7 +32,7 @@ public class Gun : MonoBehaviour
         RaycastHit hit;
         if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
         {
-            Debug.Log(hit.transform.name);
+            //Debug.Log(hit.transform.name);
 
             Target target = hit.transform.GetComponent<Target>();
             if (target != null)
